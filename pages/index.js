273d79ecbdc6014aab13c79fd0ff5d7a4c15048d
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2020-04-09 00:17:34 -0400

    Initializing AbeCares

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const Home = () => (
+	<div className="container">
+		<Head>
+			<title>Abraham Home Care</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+
+		<main>
+			Main Content
+		</main>
+
+		<footer>
+			Footer
+		</footer>
+  </div>
+)
+
+export default Home
