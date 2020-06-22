8b54d48c68871591c8c9fc6ed9e76078debb3021
ECWireless <Elliott@valtechcreative.com>
2020-06-22 17:47:05 -0600

    ApplicationForm: add Qualifications section

--- components/ApplicationForm.js
+++ components/ApplicationForm.js
@@ -28,6 +28,21 @@ export default class ApplicationForm extends Component {
         birthDay: '',
         birthYear: '',
         isCitizen: false,
+
+        employmentDesired: '',
+        startDate: '',
+        salary: '',
+
+        highSchool: '',
+        highSchoolGraduated: false,
+        college: '',
+        collegeDegree: '',
+        collegeGraduated: false,
+        graduateSchool: '',
+        graduateDegree: '',
+        graduateSchoolGraduated: false,
+
+        qualifications: '',
     }
 
     onNameChange = (e) => { this.setState({name: e.target.value}) }
@@ -41,7 +56,22 @@ export default class ApplicationForm extends Component {
     onBirthMonthChange = (e) => { this.setState({birthMonth: e.target.value}) }
     onBirthDayChange = (e) => { this.setState({birthDay: e.target.value}) }
     onBirthYearChange = (e) => { this.setState({birthYear: e.target.value}) }
-    onIsCitizenChange  = () => { this.setState({isCitizen: !this.state.isCitizen}) }
+    onIsCitizenChange = () => { this.setState({isCitizen: !this.state.isCitizen}) }
+
+    onEmploymentDesiredChange = (e) => { this.setState({employmentDesired: e.target.value}) }
+    onStartDateChange = (e) => { this.setState({startDate: e.target.value}) }
+    onSalaryChange = (e) => { this.setState({salary: e.target.value}) }
+
+    onHighSchoolChange = (e) => { this.setState({highSchool: e.target.value}) }
+    onHighSchoolGraduatedChange = () => { this.setState({highSchoolGraduated: !this.state.highSchoolGraduated}) }
+    onCollegeChange = (e) => { this.setState({college: e.target.value}) }
+    onCollegeDegreeChange = (e) => { this.setState({collegeDegree: e.target.value}) }
+    onCollegeGraduatedChange = () => { this.setState({collegeGraduated: !this.state.collegeGraduated}) }
+    onGraduateSchoolChange = (e) => { this.setState({graduateSchool: e.target.value}) }
+    onGraduateDegree = (e) => { this.setState({graduateDegree: e.target.value}) }
+    onGraduateSchoolGraduatedChange = () => { this.setState({graduateSchoolGraduated: !this.state.graduateSchoolGraduated}) }
+
+    onQualificationsChange = (e) => { this.setState({qualifications: e.target.value}) }
 
     onSubmit = (e) => {
         e.preventDefault();
@@ -58,6 +88,21 @@ export default class ApplicationForm extends Component {
             Birth Day: ${this.state.birthDay}
             Birth Year: ${this.state.birthYear}
             Is Citizen?: ${this.state.isCitizen}
+
+            Employment Desired: ${this.state.employmentDesired}
+            Start Date: ${this.state.startDate}
+            Salary Desired: ${this.state.salary}
+
+            High School: ${this.state.highSchool}
+            Did you graduate?: ${this.state.highSchoolGraduated}
+            College: ${this.state.college}
+            College Degree: ${this.state.collegeDegree}
+            Did you graduate?: ${this.state.collegeGraduated}
+            Graduate School: ${this.state.graduateSchool}
+            Graduate School Degree: ${this.state.graduateDegree}
+            Did you graduate?: ${this.state.graduateSchoolGraduated}
+
+            Qualifications: ${this.state.qualifications}
         `)
         // this.setState({ loading: true })
         // fetch('https://abecares-backend.herokuapp.com/send',{
@@ -104,7 +149,7 @@ export default class ApplicationForm extends Component {
                     className="container" onSubmit={this.onSubmit}
                 >
                     <div className="form">
-                        <h3 style={{textAlign: 'center', marginTop: '4rem'}} className="h3 white">Personal Information:</h3>
+                        <h3 style={{textAlign: 'center', marginTop: '6rem'}} className="h3 white">Personal Information:</h3>
                         <label htmlFor="application__input-name" className="form__label p-s">Full Name</label>
                         <input required={this.state.required} type="text" id="application__input-name" className="form__input p-m"
                             value={this.state.name} onChange={this.onNameChange.bind(this)}
@@ -120,7 +165,7 @@ export default class ApplicationForm extends Component {
                             <input required={this.state.required} type="text" id="application__input-city" className="form__input-medium p-m" style={{marginTop: '1rem'}}
                                 placeholder="City" value={this.state.city} onChange={this.onCityChange.bind(this)}
                             />
-                            <select name="state" id="state" className="form__input-small p-s" style={{margin: '1rem 0 0 1rem'}}
+                            <select required={this.state.required} name="state" id="state" className="form__input-small p-s" style={{margin: '1rem 0 0 1rem'}}
                                 value={this.state.americanState} onChange={this.onAmericanStateChange.bind(this)}
                             >
                                 {states.map(state=><option key={state.abbreviation}>{state.abbreviation}</option>)}
@@ -139,17 +184,17 @@ export default class ApplicationForm extends Component {
                         />
                         <label htmlFor="application__input-birth-month" className="form__label p-s">Date of Birth</label>
                         <div className="form__input-container">
-                            <select name="month" id="month" className="form__input-medium p-s" style={{margin: '1rem 0 0 0'}}
+                            <select required={this.state.required} name="month" id="month" className="form__input-medium p-s" style={{margin: '1rem 0 0 0'}}
                                 value={this.state.birthMonth} onChange={this.onBirthMonthChange.bind(this)}
                             >
                                 {months.map(month=><option key={month.abbreviation}>{month.name}</option>)}
                             </select>
-                            <select name="day" id="day" className="form__input-small p-s" style={{margin: '1rem 0 0 1rem'}}
+                            <select required={this.state.required} name="day" id="day" className="form__input-small p-s" style={{margin: '1rem 0 0 1rem'}}
                                 value={this.state.birthDay} onChange={this.onBirthDayChange.bind(this)}
                             >
                                 {days.map(day=><option key={day}>{day}</option>)}
                             </select>
-                            <select name="year" id="year" className="form__input-small p-s" style={{margin: '1rem 0 0 1rem'}}
+                            <select required={this.state.required} name="year" id="year" className="form__input-small p-s" style={{margin: '1rem 0 0 1rem'}}
                                 value={this.state.birthYear} onChange={this.onBirthYearChange.bind(this)}
                             >
                                 {years.map(year=><option key={year}>{year}</option>)}
@@ -158,21 +203,91 @@ export default class ApplicationForm extends Component {
                         <label htmlFor="application__input-citizenship" className="form__label p-s">Are you a U.S. citizen?</label>
                         <div className="form__input-container">
                             <input required={this.state.required} type="checkbox" id="application__input-citizenship" className="form__checkbox p-m"
-                                style={{marginBottom: '4rem'}} value={this.state.isCitizen} onChange={this.onIsCitizenChange}
+                                style={{marginBottom: '6rem'}} value={this.state.isCitizen} onChange={this.onIsCitizenChange}
                             />
                         </div>
                     </div>
 
-                    <div className="form" style={{marginTop: '4rem'}} onSubmit={this.onSubmit}>
-                        <h3 style={{textAlign: 'center', marginTop: '4rem'}} className="h3 white">Employment Desired:</h3>
-                        <label htmlFor="application__input-name" className="form__label p-s">Full Name</label>
-                        <input required={this.state.required} type="text" id="application__input-name" className="form__input p-m"
+                    <div className="form" style={{marginTop: '4rem'}}>
+                        <h3 style={{textAlign: 'center', marginTop: '6rem'}} className="h3 white">Employment Desired:</h3>
+                        <label htmlFor="application__input-employment-desired" className="form__label p-s">Position Applying For</label>
+                        <div className="form__input-container">
+                            <select required={this.state.required} name="employment desired" id="application__input-employment-desired" className="form__input-medium p-s" style={{marginTop: '1rem'}}
+                                value={this.state.employmentDesired} onChange={this.onEmploymentDesiredChange}
+                            >
+                                <option>Part-Time</option>
+                                <option>Full-Time</option>
+                            </select>
+                        </div>
+                        <label htmlFor="application__input-start-date" className="form__label p-s">Date You Can Start</label>
+                        <div className="form__input-container">
+                            <input required={this.state.required} type="date" id="application__input-start-date" className="form__input-medium p-s" style={{marginTop: '1rem'}}
+                                value={this.state.startDate} onChange={this.onStartDateChange.bind(this)}
+                            />
+                        </div>
+                        <label htmlFor="application__input-salary" className="form__label p-s">Desired Salary</label>
+                        <div className="form__input-container" style={{marginBottom: '6rem'}}>
+                            <input required={this.state.required} type="text" id="application__input-salary" className="form__input-medium p-m"
+                                value={this.state.salary} onChange={this.onSalaryChange.bind(this)}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form" style={{marginTop: '4rem'}}>
+                        <h3 style={{textAlign: 'center', marginTop: '6rem'}} className="h3 white">Education:</h3>
+                        <label htmlFor="application__input-high-school" className="form__label p-s">High School</label>
+                        <input required={this.state.required} type="text" id="application__input-high-school" className="form__input p-m"
+                            value={this.state.highSchool} onChange={this.onHighSchoolChange.bind(this)}
                         />
+                        <label htmlFor="application__input-high-school-graduated" className="form__label p-s">Did you graduate?</label>
+                        <div className="form__input-container">
+                            <input required={this.state.required} type="checkbox" id="application__input-high-school-graduated" className="form__checkbox p-m"
+                                value={this.state.highSchoolGraduated} onChange={this.onHighSchoolGraduatedChange}
+                            />
+                        </div>
+                        <label htmlFor="application__input-college" className="form__label p-s">College</label>
+                        <input type="text" id="application__input-college" className="form__input p-m"
+                            value={this.state.college} onChange={this.onCollegeChange.bind(this)}
+                        />
+                        <label htmlFor="application__input-college-degree" className="form__label p-s">Area of Study/Degree</label>
+                        <input type="text" id="application__input-college-degree" className="form__input p-m"
+                            value={this.state.collegeDegree} onChange={this.onCollegeDegreeChange.bind(this)}
+                        />
+                        <label htmlFor="application__input-college-graduated" className="form__label p-s">Did you graduate?</label>
+                        <div className="form__input-container">
+                            <input type="checkbox" id="application__input-college-graduated" className="form__checkbox p-m"
+                                value={this.state.collegeGraduated} onChange={this.onCollegeGraduatedChange}
+                            />
+                        </div>
+                        <label htmlFor="application__input-graduate-school" className="form__label p-s">Graduate School</label>
+                        <input type="text" id="application__input-graduate-school" className="form__input p-m"
+                            value={this.state.graduateSchool} onChange={this.onGraduateSchoolChange.bind(this)}
+                        />
+                        <label htmlFor="application__input-graduate-degree" className="form__label p-s">Area of Study/Degree</label>
+                        <input type="text" id="application__input-graduate-degree" className="form__input p-m"
+                            value={this.state.graduateDegree} onChange={this.onGraduateDegree.bind(this)}
+                        />
+                        <label htmlFor="application__input-graduate-school-graduated" className="form__label p-s">Did you graduate?</label>
+                        <div className="form__input-container">
+                            <input type="checkbox" id="application__input-graduate-school-graduated" className="form__checkbox p-m"
+                                style={{marginBottom: '6rem'}} value={this.state.graduateSchoolGraduated} onChange={this.onGraduateSchoolGraduatedChange}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="form" style={{marginTop: '4rem'}}>
+                        <h3 style={{textAlign: 'center', marginTop: '6rem'}} className="h3 white">Skills/Qualifications:</h3>
+                        <label htmlFor="application__input-qualifications" className="form__label p-s">Which relevant certifications or qualifications do you have?</label>
+                        <textarea required={this.state.required} type="text" id="application__input-qualifications" className="form__textarea p-m"
+                            value={this.state.qualifications} onChange={this.onQualificationsChange.bind(this)}
+                        />
+
                         <button style={{marginBottom: '5rem'}} type="submit" id="contact__button" className="button button__l">
                             Submit
                         </button>
-                        { this.state.submitted && <Snackbar success={this.state.success} onCloseSnackbar={this.onCloseSnackbar} /> }
                     </div>
+
+                    { this.state.submitted && <Snackbar success={this.state.success} onCloseSnackbar={this.onCloseSnackbar} /> }
                 </form>
             </React.Fragment>
         )
