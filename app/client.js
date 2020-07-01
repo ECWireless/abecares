d4c862084dfa728da090f6c0ec399cf8db6934d2
ECWireless <Elliott@valtechcreative.com>
2020-07-01 16:53:12 -0600

    Token Test 4

--- app/client.js
+++ app/client.js
@@ -1,18 +1,10 @@
 // client.js
 import sanityClient from '@sanity/client'
+import myToken from './config'
 
-const client = (props) => {
-    sanityClient({
-        projectId: 'alezxehy', // you can find this in sanity.json
-        dataset: 'production', // or the name you chose in step 1
-        useCdn: false, // `false` if you want to ensure fresh data'
-        token: props.getToken
-        
-    })
-}
-
-export async function getStaticProps() {
-    getToken = await process.env.SANITY_TOKEN
-}
-
-export default client
+export default sanityClient({
+    projectId: 'alezxehy', // you can find this in sanity.json
+    dataset: 'production', // or the name you chose in step 1
+    useCdn: false, // `false` if you want to ensure fresh data'
+    token: myToken()
+})
