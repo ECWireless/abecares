e1297a5a46fd785317cf7405eb5a99624e222860
ECWireless <Elliott@valtechcreative.com>
2021-05-16 22:35:26 -0600

    Remove plural testimonials

--- app/containers/home/About.js
+++ app/containers/home/About.js
@@ -12,7 +12,7 @@ export default function About(props) {
                     <h2 className="h2" style={{color: '#fff'}}>Thorough in knowledge, diverse in practice</h2>
                     <p className="p-s" id="about__paragraph">Abraham Home Care was founded in 2014 by owner Dwayne Jordan, who felt compelled to start his own business following an accident that left his 17-year-old nephew a quadripliegic. After a year of recuperating at the Children's Institute of Pittsburgh, his nephew was eventually sent home, where he was required 24-hour care. Dwayne noticed right away the effect of qualified, trustworthy, and compassionate home health aides on his nephew/s recovery. This experience inspired him to help others to receive similar quality home care and manage their day-to-day living with dignity and respect.</p>
                     <p style={{marginTop: '3rem'}} className="p-s" id="about__paragraph">Currently, Abraham Home Care provides services to 30 patients and employs a team of over 50 direct care workers.</p>
-                    <button className="button button__l" id="about__button" onClick={props.scrollToLocation.bind(this, 'testimonials')}>Read Testimonials</button>
+                    <button className="button button__l" id="about__button" onClick={props.scrollToLocation.bind(this, 'testimonials')}>Read Testimonial</button>
                 </div>
             <div id="about__photo-container">
                 {/* <img id="about__photo" src="/home-care-photo-2.jpg" alt="Home Care Photo"/> */}
